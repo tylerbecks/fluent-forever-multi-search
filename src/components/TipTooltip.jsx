cf1078bd88db7608fb0b157db7060abf26ef7f36
Tyler Becks <tylerdbecks@gmail.com>
2019-11-05 12:44:36 -0800

    Add tips for easily confounded images

--- src/components/TipTooltip.jsx
+++ src/components/TipTooltip.jsx
@@ -4,19 +4,25 @@ import injectSheet from "react-jss"
 
 const styles = {
   icon: {
-    float: 'right',
-    fontSize: [['0.6em'], '!important']
-  }
+    float: "right",
+    fontSize: [["0.6em"], "!important"],
+  },
 }
 
 const CATEGORY_MESSAGE =
   "Learn these words word by using 2-3 other pictures/words on your flashcards (i.e. ‘animal = dog, cat, fish…’)"
+const CONFOUNDED_IMAGES_MESSAGE =
+  " These are groups of words that will use very similar images (girl/daughter, marriage/wedding). Learn these words by adding a personal touch (i.e., the name of a ‘daughter’ you might know) or an additional word or two in your target language (i.e., daughter might go with mother/father)."
 
 const TipTooltip = ({ classes, tip }) => {
-  let content;
-  if (tip === 'Category Word') {
+  let content
+  if (tip === "Category Word") {
     content = CATEGORY_MESSAGE
   }
+  if (tip === 'Easily Confounded Images') {
+    content = CONFOUNDED_IMAGES_MESSAGE
+  }
+
   return (
     <Popup
       trigger={<Icon circular name="info" className={classes.icon} />}

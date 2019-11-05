ce8d6233601ffabdebf3b7828bac46b7e8a71a46
Tyler Becks <tylerdbecks@gmail.com>
2019-11-04 22:14:34 -0800

    Hoist state up to index

--- src/components/WordSearchTool.jsx
+++ src/components/WordSearchTool.jsx
@@ -1,37 +1,24 @@
-import React, { useState, useEffect } from "react"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react"
 import { Button, Card, Header, Label, Progress, Flag } from "semantic-ui-react"
 import { searchWord } from "../utils/seachWord"
-import { getItem, setItem } from "../utils/store"
 
-const STORE_INDEX_KEY = "currentWordIndex"
-let englishWords
-
-export default ({ language }) => {
-  const [index, setIndex] = useState(getInitialIndex())
-  const [translatedWord, setTranslatedWord] = useState("")
-  let englishWord = englishWords && englishWords[index].word
-
-  useEffect(() => {
-    ;(async () => {
-      const translatedWord = await fetchTranslatedWord(englishWord, language)
-      setTranslatedWord(translatedWord)
-    })()
-  }, [index, language])
-
+export default ({
+  englishWord,
+  hint,
+  index,
+  language,
+  onChangeIndex,
+  totalWordCount,
+  translatedWord,
+}) => {
   const handleClickNext = () => {
-    if (index === englishWords.length - 1) return
-    changeWord(index + 1)
+    if (index === totalWordCount - 1) return
+    onChangeIndex(index + 1)
   }
 
   const handleClickPrev = () => {
     if (index === 0) return
-    changeWord(index - 1)
-  }
-
-  const changeWord = newIndex => {
-    setIndex(newIndex)
-    setItem(STORE_INDEX_KEY, newIndex)
+    onChangeIndex(index - 1)
   }
 
   const handleSearch = () => {
@@ -39,95 +26,56 @@ export default ({ language }) => {
   }
 
   const isPrevDisabled = () => index === 0
-  const isNextDisabled = () => index === englishWords.length - 1
+  const isNextDisabled = () => index === totalWordCount - 1
 
   return (
-    <StaticQuery
-      query={graphql`
-        query AllWordsJson {
-          allFrequentWordsJson {
-            edges {
-              node {
-                word
-                hint
-              }
-            }
-          }
-        }
-      `}
-      render={data => {
-        if (!englishWords) {
-          englishWords = data.allFrequentWordsJson.edges.map(({ node }) => node)
-        }
+    <Card raised>
+      <Progress
+        attached="top"
+        color="green"
+        total={totalWordCount}
+        value={index + 1}
+      />
+      <Progress
+        attached="bottom"
+        color="green"
+        total={totalWordCount}
+        value={index + 1}
+      />
+      <Card.Content>
+        <Header textAlign="left">
+          <Flag name="us" />
+          {englishWord}
+          {hint && <Label content={hint} color="green" circular />}
+        </Header>
+        <Header textAlign="left">
+          <Flag name={language} />
+          {translatedWord}
+        </Header>
+      </Card.Content>
 
-        const { hint, word: englishWord } = englishWords[index]
-
-        return (
-          <Card raised>
-            <Progress
-              attached="top"
-              color="green"
-              total={englishWords.length}
-              value={index + 1}
-            />
-            <Progress
-              attached="bottom"
-              color="green"
-              total={englishWords.length}
-              value={index + 1}
-            />
-            <Card.Content>
-              <Header textAlign="left">
-                <Flag name="us" />
-                {englishWord}
-                {hint && <Label content={hint} color="green" circular />}
-              </Header>
-              <Header textAlign="left">
-                <Flag name={language} />
-                {translatedWord}
-              </Header>
-            </Card.Content>
-
-            <Card.Content extra>
-              <Button.Group>
-                <Button
-                  icon="angle double left"
-                  disabled={isPrevDisabled()}
-                  onClick={handleClickPrev}
-                  type="button"
-                />
-                <Button
-                  content="Search!"
-                  color="orange"
-                  type="submit"
-                  onClick={handleSearch}
-                />
-                <Button
-                  icon="angle double right"
-                  disabled={isNextDisabled()}
-                  onClick={handleClickNext}
-                  type="button"
-                />
-              </Button.Group>
-            </Card.Content>
-          </Card>
-        )
-      }}
-    />
+      <Card.Content extra>
+        <Button.Group>
+          <Button
+            icon="angle double left"
+            disabled={isPrevDisabled()}
+            onClick={handleClickPrev}
+            type="button"
+          />
+          <Button
+            content="Search!"
+            color="orange"
+            type="submit"
+            onClick={handleSearch}
+          />
+          <Button
+            icon="angle double right"
+            disabled={isNextDisabled()}
+            onClick={handleClickNext}
+            type="button"
+          />
+        </Button.Group>
+      </Card.Content>
+    </Card>
   )
-}
-
-const getInitialIndex = () => {
-  const storedIndex = Number(getItem(STORE_INDEX_KEY))
-  return storedIndex || 0
-}
-
-const fetchTranslatedWord = async (englishWord, targetLanguage) => {
-  const response = await fetch(
-    `https://translation.googleapis.com/language/translate/v2?key=${process.env.GATSBY_GOOGLE_TRANSLATE_API_KEY}&q=${englishWord}&target=${targetLanguage}&source=en`
-  )
-  const json = await response.json()
-  const translation = json.data.translations[0].translatedText
-
-  return translation
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,97 @@
 import "semantic-ui-css/semantic.min.css"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import { StaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import WordSearchTool from "../components/WordSearchTool"
 import SEO from "../components/seo"
-import { getItem, setItem } from '../utils/store'
+import { getItem, setItem } from "../utils/store"
 
 const STORE_LANGUAGE_KEY = "language"
+const STORE_INDEX_KEY = "currentWordIndex"
+
+let englishWords
 
 const IndexPage = () => {
   const [language, setLanguage] = useState(getDefaultLanguage())
+  const [index, setIndex] = useState(getInitialIndex())
+  const [translatedWord, setTranslatedWord] = useState("")
+  let englishWord = englishWords && englishWords[index].word
+
+  useEffect(() => {
+    ;(async () => {
+      const newTranslatedWord = await fetchTranslatedWord(englishWord, language)
+      setTranslatedWord(newTranslatedWord)
+    })()
+  }, [index, language])
 
   const onChangeLanguage = newLanguage => {
     setLanguage(newLanguage)
     setItem(STORE_LANGUAGE_KEY, newLanguage)
   }
 
+  const onChangeIndex = (newIndex) => {
+    setIndex(newIndex)
+    setItem(STORE_INDEX_KEY, newIndex)
+  }
+
   return (
-    <Layout language={language} onChangeLanguage={onChangeLanguage}>
-      <SEO title="Home" />
-      <WordSearchTool language={language} />
-    </Layout>
+    <StaticQuery
+      query={graphql`
+        query AllWordsJson {
+          allFrequentWordsJson {
+            edges {
+              node {
+                word
+                hint
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        if (!englishWords) {
+          englishWords = data.allFrequentWordsJson.edges.map(({ node }) => node)
+        }
+
+        const { hint, word: englishWord } = englishWords[index]
+
+        return (
+          <Layout language={language} onChangeLanguage={onChangeLanguage}>
+            <SEO title="Home" />
+            <WordSearchTool
+              englishWord={englishWord}
+              hint={hint}
+              index={index}
+              language={language}
+              onChangeIndex={onChangeIndex}
+              totalWordCount={englishWords.length}
+              translatedWord={translatedWord}
+            />
+          </Layout>
+        )
+      }}
+    />
   )
 }
 
 const getDefaultLanguage = () => {
   const storedLanguage = getItem(STORE_LANGUAGE_KEY)
   return storedLanguage || "es"
+}
+
+const getInitialIndex = () => {
+  const storedIndex = Number(getItem(STORE_INDEX_KEY))
+  return storedIndex || 0
+}
+
+const fetchTranslatedWord = async (englishWord, targetLanguage) => {
+  const response = await fetch(
+    `https://translation.googleapis.com/language/translate/v2?key=${process.env.GATSBY_GOOGLE_TRANSLATE_API_KEY}&q=${englishWord}&target=${targetLanguage}&source=en`
+  )
+  const json = await response.json()
+  const translation = json.data.translations[0].translatedText
+
+  return translation
 }
 
 export default IndexPage

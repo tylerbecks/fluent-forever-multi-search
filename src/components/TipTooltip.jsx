8ff959456c9f46709c4a120ec86629ea4ee30546
Tyler Becks <tylerdbecks@gmail.com>
2019-11-05 12:54:09 -0800

    Add tips for pronouns

--- src/components/TipTooltip.jsx
+++ src/components/TipTooltip.jsx
@@ -15,17 +15,26 @@ const CONFOUNDED_IMAGES_MESSAGE =
   "These are groups of words that will use very similar images (girl/daughter, marriage/wedding). Learn these words by adding a personal touch (i.e., the name of a ‘daughter’ you might know) or an additional word or two in your target language (i.e., daughter might go with mother/father)."
 const TIME_MESSAGE =
   "If needed, define each time division in terms of another time division, i.e. 60 x minuto = 1 ___ (ora), 1 ora = 60 x ____ (minuto)."
+const PRONOUN_MESSAGE =
+  "Make sure you read about these in your grammar book before adding them. Languages divide their pronouns into many categories. Hungarian, for instance, has six words for “you” (singular informal, singular formal (for acquaintances), singular official (for teachers, policemen, bureaucrats), plural informal, etc.), and depending upon how you count, Japanese either has no pronouns or tons of pronouns. We’ll need to have some pronouns now in order to deal with grammar later, so you’ll want to find at least a few words to refer to yourself or someone else. You’ll find a good explanation of pronouns (and a list of them) in the beginning of your grammar book. Note that you don’t yet need him, her, his, their, etc. We’ll get them later, when we discuss grammar."
 
 const TipTooltip = ({ classes, tip }) => {
   let content
-  if (tip === "Category Word") {
-    content = CATEGORY_MESSAGE
-  }
-  if (tip === "Easily Confounded Images") {
-    content = CONFOUNDED_IMAGES_MESSAGE
-  }
-  if (tip === 'Time Unit') {
-    content = TIME_MESSAGE
+  switch (tip) {
+    case "Category Word":
+      content = CATEGORY_MESSAGE
+      break
+    case "Easily Confounded Images":
+      content = CONFOUNDED_IMAGES_MESSAGE
+      break
+    case "Time Unit":
+      content = TIME_MESSAGE
+      break
+    case "Pronoun":
+      content = PRONOUN_MESSAGE
+      break
+    default:
+      throw error("tip unknown")
   }
 
   return (

d8ab6f0c350de8ff7b50356bd3706788cf13f03c
Tyler Becks <tylerdbecks@gmail.com>
2019-11-05 12:48:10 -0800

    Add tips for time units

--- src/components/TipTooltip.jsx
+++ src/components/TipTooltip.jsx
@@ -12,15 +12,20 @@ const styles = {
 const CATEGORY_MESSAGE =
   "Learn these words word by using 2-3 other pictures/words on your flashcards (i.e. ‘animal = dog, cat, fish…’)"
 const CONFOUNDED_IMAGES_MESSAGE =
-  " These are groups of words that will use very similar images (girl/daughter, marriage/wedding). Learn these words by adding a personal touch (i.e., the name of a ‘daughter’ you might know) or an additional word or two in your target language (i.e., daughter might go with mother/father)."
+  "These are groups of words that will use very similar images (girl/daughter, marriage/wedding). Learn these words by adding a personal touch (i.e., the name of a ‘daughter’ you might know) or an additional word or two in your target language (i.e., daughter might go with mother/father)."
+const TIME_MESSAGE =
+  "If needed, define each time division in terms of another time division, i.e. 60 x minuto = 1 ___ (ora), 1 ora = 60 x ____ (minuto)."
 
 const TipTooltip = ({ classes, tip }) => {
   let content
   if (tip === "Category Word") {
     content = CATEGORY_MESSAGE
   }
-  if (tip === 'Easily Confounded Images') {
+  if (tip === "Easily Confounded Images") {
     content = CONFOUNDED_IMAGES_MESSAGE
+  }
+  if (tip === 'Time Unit') {
+    content = TIME_MESSAGE
   }
 
   return (
